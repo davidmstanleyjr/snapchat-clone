d4b44d11a504beeeb81a88b0ccb4d082333bad2a
davidmstanleyjr <davidmstanleyjr@gmail.com>
2021-09-02 19:35:47 -0400

    trying to see something

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyCLkegSHDmDrkgaxFzcpqiYd-xbUxgaU4A",
-	authDomain: "snapchat-clone-4bc4c.firebaseapp.com",
-	projectId: "snapchat-clone-4bc4c",
-	storageBucket: "snapchat-clone-4bc4c.appspot.com",
-	messagingSenderId: "567301089644",
-	appId: "1:567301089644:web:fe5f890ac370592a87ab49",
+	apiKey: process.env.API_KEY,
+	authDomain: process.env.AUTH_DOMAIN,
+	projectId: process.env.PROJECT_ID,
+	storageBucket: process.env.STORAGE_BUCKET,
+	messagingSenderId: process.env.MESSAGING_SENDER_ID,
+	appId: process.env.APP_ID,
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
